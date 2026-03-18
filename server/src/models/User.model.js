cedaf817554719b76d1fcddd0110e589beceaeec
Pham Hoang Minh - 1954 <hoangminh.p7979@gmail.com>
2026-03-18 12:23:03 +0700

    Tạo model User và API auth và phân quyền

---
+++ server/src/models/User.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "seller", "admin"],
+      default: "user",
+    },
+
+    phone: String,
+    avatar: String,
+
+    shopId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+
+    isSellerRequested: {
+      type: Boolean,
+      default: false,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
